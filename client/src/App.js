c0311a805af38404c31ecfe10e87d1bb5df875b7
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2020-06-15 17:10:19 +0600

    done with client and reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import AppNavbar from './components/AppNavbar'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <AppNavbar/>
     </div>
   );
 }
