cefdd671ff80a83286a93b41a4db88714aa8c785
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2020-06-30 22:18:42 +0600

    updated and deploying to heroku

--- README.md
+++ README.md
@@ -10,3 +10,28 @@ This is just a demonstration in order to know how the MERN stack works. How we c
 - **MongoDB, React, Express**
 
 - We can use it as a todo-list app to. 
+
+## Update
+
+- We are deploying it on heroku, as it is a full stack app we need to run both side at the same time. like **server** and **client** at the same time. 
+
+- We configured the _server.js_ and added a script on server's config. Which defines that we need to run the build file for client side files if the production build isn't run yet. 
+
+```json
+"heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
+```
+
+- Server configuration that checks new builds on our every push. 
+
+```js
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // finds the index.html file from the build folder of the client side. 
+  app.get('*', () => {
+    resizeBy.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+```
--- server.js
+++ server.js
@@ -1,30 +1,40 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
-// add the api 
+// add the api
 const items = require('./routes/api/items');
 
 const app = express();
 
-// Bodyparser Middleware 
+// Bodyparser Middleware
 app.use(bodyParser.json());
 
-// DB config 
+// DB config
 const db = require('./config/keys').mongoURI;
 
-// Connect to mongo 
+// Connect to mongo
 // this is promise based
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
-// use routes 
+// use routes
 // anything that goes to api/items will refer to items variable
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', () => {
+    resizeBy.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
