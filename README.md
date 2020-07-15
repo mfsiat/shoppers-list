f92525d86568e8be6da7ef29d9049a1677e0dbab
Md. Nasirul Islam <nasirul.islam@northsouth.edu>
2020-07-15 10:26:51 +0600

    added user model for jwt token

--- README.md
+++ README.md
@@ -35,3 +35,5 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 ```
+
+- Implementing JWT Auth.
---
+++ models/User.js
@@ -0,0 +1,20 @@
+// 1. bring in the DB 
+// 2. Define the DB (schema)
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema 
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// export the module
+module.exports = User = mongoose.model('user', UserSchema);
