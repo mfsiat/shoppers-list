c7d3dbba465e5a1f8e3cb3c48da7aa7d989407b8
Md. Nasirul Islam <nasirul.islam@northsouth.edu>
2020-07-19 18:06:09 +0600

    added redux auth with jwt token

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,12 @@
 // this is the meeting place for all reducers
 // every reducer we create we import them here
-import { combineReducers } from 'redux';
-import itemReducer from './itemReducer.js';
+import { combineReducers } from "redux";
+import itemReducer from "./itemReducer.js";
+import errorReducer from "./errorReducer.js";
+import authReducer from "./authReducer.js";
 
 export default combineReducers({
-  item: itemReducer
-})
+  item: itemReducer,
+  error: errorReducer,
+  auth: authReducer,
+});
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,27 +1,28 @@
 // create the middleware which handle the private route
 // user can go after authentication
 
-const config = require("config");
-const jwt = require("jsonwebtoken");
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 // middleware function
 // must pass in 3 things req, res, next
 // it gets the token that gets sent from the front end
 function auth(req, res, next) {
-  const token = req.header("x-auth-token");
+  const token = req.header('x-auth-token');
 
   // Check for token
-  if (!token) res.status(401).json({ msg: "No token, authorization denied" });
+  if (!token)
+    return res.status(401).json({ msg: 'No token, authorization denied' });
 
   try {
     // Verify token
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
 
     // Add user from payload
     req.user = decoded;
     next();
   } catch (e) {
-    res.status(400).json({ msg: "Token is not valid" });
+    res.status(400).json({ msg: 'Token is not valid' });
   }
 }
 
